c102ca9c794ea664d8b65458ea59b358ea8145fe
AmitMSZ <amitmartinez50@gmail.com>
2023-07-21 09:09:45 -0400

    build

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// eslint-disable-next-line react/prop-types
 export function TwitterFollowCard({ userName , name, initialIsFollowing}) {
 
     const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
